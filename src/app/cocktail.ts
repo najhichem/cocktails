f925c7a6181bef3d8c0ee903d6c67bed4a8b685d
unknown <najarihichem@gmail.com>
2018-03-11 15:00:31 +0100

    test

---
+++ src/app/cocktail.ts
@@ -0,0 +1,13 @@
+export class Cocktail{
+	public name: string;
+	public desc: string;
+	public img: string;
+
+	constructor(name: string, desc:string, img:string){
+		this.name = name;
+                this.desc = desc;
+                this.img = img;
+
+	}
+
+}
