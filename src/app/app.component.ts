abbb5cd2de26521f78e41539f9ffec0b223608a3
Najari Hichem <najarihichem@gmail.com>
2018-05-10 18:43:28 +0100

    ngchange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation, ViewChild, ElementRef} from '@angular/core';
 import {Cocktail} from './cocktail';
 
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    //encapsulation: ,
 })
 export class AppComponent {
     public prenom: string;
     public fruit: string;
     public fruits: string[] = [];
+    @ViewChild('f') public el: ElementRef;
 
+    public data: string = '';
+    public display = true;
+    title = 'app works';
 
     ngOnInit() {
 
         this.prenom = "Jean";
     }
 
-    title = 'app';
+    //title = 'app';
     cocktail = new Cocktail("poire", "test ", "https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg");
 
     deleteFruit(fruit: string) {
         this.fruits.splice(this.fruits.indexOf(fruit), 1)
+    }
+
+    addFruit() {
+        //console.log(ref);
+        //this.fruits.push(ref.value);
+        this.fruits.push(this.el.nativeElement.value);
     }
 }
--- src/app/fruits/fruits.component.ts
+++ src/app/fruits/fruits.component.ts
@@ -1,19 +1,60 @@
-import {Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import {
+    Component, OnInit, Input, EventEmitter, Output, ContentChild, ElementRef, OnChanges, DoCheck,
+    AfterContentInit, AfterViewInit, AfterViewChecked, AfterContentChecked, OnDestroy
+
+} from '@angular/core';
 
 @Component({
     selector: 'app-fruits',
     templateUrl: './fruits.component.html',
     styleUrls: ['./fruits.component.css']
 })
-export class FruitsComponent implements OnInit {
+export class FruitsComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterViewInit, AfterViewChecked, AfterContentChecked, OnDestroy {
 
     @Input('f') public fruit: string;
     @Output() public deleteF: EventEmitter<string> = new EventEmitter;
+    @ContentChild('p') public el: ElementRef;
 
-    constructor() {}
+    @Input('') public data: string;
+
+    constructor() {
+
+        console.log(' this data : ' + this.data);
+    }
+
+    ngOnChanges(simpleChange) {
+        console.log('Ng On Change value : ' + this.data);
+    }
+
+
 
     ngOnInit() {
+        console.log('Ng On INIT value : ' + this.data);
     }
+
+    ngDoCheck() {
+        console.log('Ng Do Check value : ' + this.data);
+    }
+
+    ngAfterViewChecked() {
+        console.log('Ng AfterViewChecked value : ' + this.data);
+    }
+
+    ngAfterViewInit() {
+        console.log('Ng AfterViewInit value : ' + this.data);
+    }
+
+    ngAfterContentInit() {
+        console.log('Ng AfterContentInit value : ' + this.data);
+    }
+
+    ngAfterContentChecked() {
+        console.log('Ng AfterContentChecked value : ' + this.data);
+    }
+    ngOnDestroy() {
+        console.log('Ng OnDestroy value : ' + this.data);
+    }
+
 
     deleteFruit() {
         this.deleteF.emit(this.fruit);
