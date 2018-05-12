95920345c1b576750691d8c5e546ee13cf1eb316
Najari Hichem <najarihichem@gmail.com>
2018-05-12 18:26:14 +0100

    add directive
    	modified:   src/app/app.component.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
-import { CocktailsDetailsComponent } from './cocktails-details/cocktails-details.component';
-import { HeaderComponent } from './header/header.component';
+import {AppComponent} from './app.component';
+import {CocktailsListComponent} from './cocktail-container/cocktails-list/cocktails-list.component';
+import {CocktailsDetailsComponent} from './cocktail-container/cocktails-details/cocktails-details.component';
+import {HeaderComponent} from './header/header.component';
+import {CocktailContainerComponent} from './cocktail-container/cocktail-container.component';
 
+import {ColorDirective} from './shared/directives/color.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CocktailsListComponent,
-    CocktailsDetailsComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CocktailsListComponent,
+        CocktailsDetailsComponent,
+        HeaderComponent,
+        CocktailContainerComponent,
+        ColorDirective
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
